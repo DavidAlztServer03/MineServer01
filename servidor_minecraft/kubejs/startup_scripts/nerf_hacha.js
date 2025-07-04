ce036393c2900f05be4ca7634692aa70473d0a1c
DavidAlztServer03 <mirswatf18@gmail.com>
2025-07-04 01:30:30 +0000

    [🌙] Backup realizado! 2025-07-04

--- servidor_minecraft/kubejs/startup_scripts/nerf_hacha.js
+++ servidor_minecraft/kubejs/startup_scripts/nerf_hacha.js
@@ -1,9 +1,9 @@
-// Archivo corregido: kubejs/startup_scripts/nerf_hacha.js
+// Archivo: kubejs/startup_scripts/nerf_hacha.js
 
 ItemEvents.modification(event => {
   event.modify('born_in_chaos_v1:supreme_measure', item => {
     item.maxDamage = 5
-    item.enchantability = 0
+    // item.enchantability = 0 // ❌ Esto no se puede modificar, rompe el script
 
     item.setAttributeModifiers([
       {
